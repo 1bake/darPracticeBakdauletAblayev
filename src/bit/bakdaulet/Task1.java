f0bef885c29cc54511a0a82bcefc8a52152c81f7
Bakdaulet <Uldana@MacBook-Pro-mak-2.local>
2022-03-03 22:13:14 +0600

    Initial commit

---
+++ src/bit/bakdaulet/Task1.java
@@ -0,0 +1,17 @@
+package bit.bakdaulet;
+
+public class Task1 {
+
+        public static  String shuffledString(String s, int[] numbers) {
+            StringBuilder stringBuilder = new StringBuilder(s);
+            for(int i =0; i < numbers.length; ++i)
+                stringBuilder.setCharAt(numbers[i],s.charAt(i));
+            return stringBuilder.toString();
+        }
+        public static void main(String[] args) {
+            String s="codeLeet";
+            int [] arr = {4,5,6,7,0,1,2,3};
+            String result= shuffledString(s,arr);
+            System.out.println(result);
+        }
+}
